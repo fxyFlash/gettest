177631e5eb24559b7b942856748adcd6df854349
fxyFlash <2361200869@qq.com>
2021-12-15 14:39:14 +0800

    TestReports

--- Test Suites/android.ts
+++ Test Suites/android.ts
@@ -11,4 +11,16 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>5e2999a2-d315-4a31-b03a-45640387ae6e</testSuiteGuid>
+   <testCaseLink>
+      <guid>f7bed9f6-5d9f-483c-a3e9-66c043ea05c8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/phone/browse</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c983c10a-9ea9-4052-a8d3-2be48ad47a46</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/phone/phonelogin</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/web.ts
+++ Test Suites/web.ts
@@ -23,4 +23,184 @@
       <isRun>true</isRun>
       <testCaseId>Test Cases/Student/Study/study</testCaseId>
    </testCaseLink>
+   <testCaseLink>
+      <guid>5531c87d-1b07-4414-832d-6ff8fb900452</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EducationServices/service</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c6bb922c-8083-4d50-b7e5-46d836c9d796</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EducationServices/InstitutionalApplication/application</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e5e65101-c91c-4217-9ab4-6278f3705749</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EducationServices/serviceprofit/serviceprofit</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b221381c-e97b-4dc3-8b85-29e4a7c3f19e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Student/Study/study</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>99a260f4-47b6-4cf5-87d6-ddafc4e1c19c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Student/PurchaseCourse/purchasecourse</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bfd6e108-1b57-40c6-9851-89a8e9e89c19</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Student/StudentApplication/studentapplication</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d6baa5b5-ac8b-4a11-9f2c-3889f02c3433</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agent/AgentApplication/application</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fc7a5e03-da79-4a1c-94e6-e38b6f00dd38</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agent/AgentClassic/agentclassic</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>de1b09d5-3e79-40e8-b368-96a210385a5e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agent/agent</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2e533df2-d0a8-406a-8a55-13dd0ac5bdf7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agent/AgentProfits/agentprofits</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c734e74f-9f39-40f7-a82e-df46be93acd0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/LiveApplication</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9b9385dd-734c-4653-8bfc-67685baeb5a2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Livelist</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8beb3247-9df6-469d-9b47-f26dd9e72af4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/TeacherClass</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5bcf8f8d-1a95-4081-93ce-47e6cb59dc9e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>efc9770d-3e85-4a5a-9d89-2f0ce7b34686</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/admincoder/sys</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a40537a1-6cd0-4da2-9934-27bb83d9f52e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EducationServices/service</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>10da4c8e-07c6-47fe-afd1-fd9ce3151a73</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EducationServices/InstitutionalApplication/application</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b2f62f31-a3ee-488f-981d-b65c2804984c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EducationServices/serviceprofit/serviceprofit</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f02af67c-81bf-45aa-8939-f1f9f2ef5a8a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Student/Study/study</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>67fff743-2c42-4429-ac38-6df2de4412b2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Student/PurchaseCourse/purchasecourse</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>01b3b740-1bd4-4ed1-8c39-d6f96fd6e4ae</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Student/StudentApplication/studentapplication</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f5919ea6-4c72-4622-bc29-ee4608570748</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agent/AgentApplication/application</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b6b5e2a3-4c5f-4dba-88bb-f5cc6040e9b2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agent/AgentClassic/agentclassic</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>772735ed-4d35-4da5-b857-58713b6d02d5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agent/agent</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>10ef330a-a45f-4878-8869-33f3be501b2e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agent/AgentProfits/agentprofits</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>36fbb3d4-0c30-4300-a40b-9f718619a6e2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/LiveApplication</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0f474916-0c22-4e32-9baa-046ceddd650c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Livelist</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d6e4aa74-6c6c-4396-bab7-c40cb0b2855e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/TeacherClass</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3583e2ce-ab4e-45c7-816b-e921fda24241</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>93c6b3e5-c855-4807-8371-539659a68d79</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/admincoder/sys</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
